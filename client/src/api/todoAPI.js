db056c63fb68dcce06366060f47b6f2df1ba3049
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2025-01-31 17:59:47 +0200

    Create todoAPI.js

---
+++ client/src/api/todoAPI.js
@@ -0,0 +1,26 @@
+import { del, get, post, put } from "./requester";
+
+export async function  getAllTodos() {
+    const todos = get('data/todo');
+    return todos;
+}
+
+export async function getSingleTodo(id) {
+    const todo = await get(`data/todo/${id}`);
+    return todo;
+}
+
+export async function  createTodo(data) {
+    const createdTodo = await post('data/todo', data);
+    return createdTodo;
+}
+
+export async function updateTodo(id, data) {
+    const todo = await put(`data/todo/${id}`, data);
+    return todo;
+}
+
+export async function  deleteTodo(id) {
+    const deleted = await del(`data/todo/${id}`);
+    return deleted;
+}
