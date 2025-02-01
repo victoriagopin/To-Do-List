fa6f7f5b790959e79ad71659df2b441757f84d70
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2025-02-01 16:03:48 +0200

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
 import { formatDate } from "../../heplers/dateFormatter";
-import Login from "../login/Login";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Header(){
       const date = formatDate();
+      const {user} = useContext(UserContext);
 
     return (
         <header>
-        <div className="auth">
-           <Link to="/login">Log In</Link>
-           <Link to="/register">Register</Link>
-        </div>
+            {user ? 
+            <>
+            <div className="auth">
+                <Link to="" className="logout">Log Out</Link>
+            </div>
+            <p className="date">Date: {date.day}.{date.formattedMonth}.{date.year}</p>
+            </>
+            :
+            (<>    
+            <div className="auth">
+                <Link to="/login">Log In</Link>
+                <Link to="/register">Register</Link>
+            </div>
+        </>)}
         <h1>To Do List</h1>
-        <p className="date">Date: {date.day}.{date.formattedMonth}.{date.year}</p>
+        
     </header>
     )
 }
