c6840952bca24cb11420aeaf53e038ebf0368219
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2025-02-01 15:42:33 +0200

    Added on login functionality

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,15 +1,43 @@
+import { useContext, useState} from 'react'
+import { useNavigate} from "react-router-dom";
 import style from './Login.module.css'
+import { post } from '../../api/requester'
+import { UserContext } from '../../contexts/UserContext';
 
 export default function Login(){
+    const {setUser} = useContext(UserContext);
+    const [values, setValues] = useState({
+        email : '',
+        password : ''
+    });
+    const navigate = useNavigate();
+
+    const onChangeHandler = (e) =>{
+        setValues(prevValues => ({
+            ...prevValues,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const onSubmit = async (e) =>{
+        e.preventDefault();
+         try{
+            const res = await post('users/login', values)
+            setUser(res);
+            navigate('/')
+         } catch (err){
+            console.log(err.message);
+         }
+    }
+
     return(
- 
                <main>
-                    <form className={style['form-login']}>
+                    <form className={style['form-login']} onSubmit={onSubmit}>
                         <label htmlFor="email" className={style.email}>
-                            <p className={style.email}>Email:</p><input type="text" name="email" className={style.email}/>
+                            <p className={style.email}>Email:</p><input type="text" name="email" className={style.email} value={values.email} onChange={onChangeHandler}/>
                         </label>
                         <label htmlFor="password" className={style.pass}>
-                            <p className={style.pass}>Password:</p><input type="password" name="password" className={style.pass}/>
+                            <p className={style.pass}>Password:</p><input type="password" name="password" className={style.pass} value={values.password} onChange={onChangeHandler}/>
                         </label>
                         <button className={style['login-btn']}>Log In</button>
                     </form>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
+import { UserProvider } from './contexts/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
     </BrowserRouter>
 
 )
