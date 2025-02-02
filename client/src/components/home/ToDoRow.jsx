78ca2b0488e6c8c2d5fd1e2727c20acc2482604f
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2025-02-02 16:59:55 +0200

    Added delete functionality

--- client/src/components/home/ToDoRow.jsx
+++ client/src/components/home/ToDoRow.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { updateTodo } from "../../api/todoAPI"
+import { deleteTodo, updateTodo } from "../../api/todoAPI"
 import { UserContext } from "../../contexts/UserContext";
 
 export default function ToDoRow({todo, refreshTodos}){
@@ -17,12 +17,29 @@ export default function ToDoRow({todo, refreshTodos}){
              refreshTodos();
     }
 
+    const onDelete = async (e) => {
+        const id = e.target.parentElement.id;
+
+        try{
+            const hasConfirmed = confirm(`Are you sure you want to delete this to-do?`);
+            if(hasConfirmed){
+            await deleteTodo(id);
+            } else{
+                return;
+            }
+        } catch (err){
+            console.log(err.message)
+        }
+        
+        refreshTodos();
+    }
+
     return(
         <li>{todo.action}
         <div className="done-not" id={todo._id}>
             <i className="fas fa-check tick" onClick={onCheck}></i>
             <i className="fas fa-edit pen"></i>
-            <i className="fas fa-times x"></i>
+            <i className="fas fa-times x" onClick={onDelete}></i>
         </div>
         </li>
     )
