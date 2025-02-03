4a06d8805e7ba640a25a9409d60612cd5c456e9d
Victoria Gopin <145561051+victoriagopin@users.noreply.github.com>
2025-02-03 14:04:09 +0200

    Added Edit functionality

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,14 +1,24 @@
 import { useContext, useEffect, useState } from 'react';
 import style from './Edit.module.css'
-import { getAllTodos } from '../../api/todoAPI';
+import { getAllTodos, updateTodo } from '../../api/todoAPI';
 import { UserContext } from '../../contexts/UserContext';
 import ToDoRow from "../home/ToDoRow";
 import DoneToDos from '../home/DoneToDos';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function Edit(){
-    const {user} = useContext(UserContext)
+    const {user} = useContext(UserContext);
+    const {id} = useParams();
     const [toDos, setToDos] = useState([]);
+    const [todoToEdit, setToDoToEdit] = useState({});
     const [doneOnes, setDoneOnes] = useState([]);
+    const [values, setValues] = useState({
+        action: todoToEdit?.action || '',
+        _ownerId: user?._id,
+        status: todoToEdit?.status || 'not done',
+        _id: id,
+    });
+    const navigate = useNavigate();
     
        
     useEffect(() => {
@@ -18,23 +28,44 @@ export default function Edit(){
     
               const done = response.filter(todo => todo.status == 'done' && todo._ownerId == user._id);
               const notDone = response.filter(todo => todo.status == 'not done' && todo._ownerId == user._id);
-    
+              const editTodo = response.find((todo) => todo._id == id)  
+
                 setToDos(notDone);
                 setDoneOnes(done);
-    
+                setToDoToEdit(editTodo);
+                setValues(editTodo);
             } catch (err){
                 console.log(err.message);
               }
         })();
     },[]);
 
+    const onChangeHandler = (e) => {
+        setValues(prevValues => ({
+            ...prevValues,
+            [e.target.name] : e.target.value
+        }));
+    }
+
+    const onEdit = async(e) => {
+        e.preventDefault();
+            try{
+                await updateTodo(id, values);
+                navigate('/')
+
+            } catch (err){
+                    console.log(err.message);
+            }   
+    }
+ 
+
     return(
         <main>
             <div className="add-todo">
-            <form>
+            <form onSubmit={onEdit}>
                 <h2>Edit To Do:</h2>
                 <label htmlFor="add-todo">
-                    <input type="text" name="action"/>
+                    <input type="text" name="action" value={values.action || ''} onChange={onChangeHandler}/>
                     <button>Edit</button>
                 </label>
             </form>
